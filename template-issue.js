e79ca6e5cc47242b45ea2312532a49760d8ddadc
Craig Levy <craig@profabelectronics.com>
2015-01-23 14:33:23 +0000

    Finished example.

--- template-issue.js
+++ template-issue.js
@@ -1,23 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    thisShouldOutputHello: function () {
+      // this doesn't work inside the contentblock
+      return Template.instance().customProp.get();
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
+  Template.hello.created = function () {
+    this.customProp = new ReactiveVar('hello');
+  }
 }
